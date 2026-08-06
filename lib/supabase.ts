edd368cec411defbe2cfc2473042405c04000e10
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-08-06 13:47:05 -0300

    Atualizar o supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://rnekatkvfhdkyiyvnfjo.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJuZWthdGt2Zmhka3lpeXZuZmpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwMjQ5MTIsImV4cCI6MjEwMTYwMDkxMn0.DpO9D2mPhJSZPbqHnVpDoZtLceCvm7yZy6mLOiDV8Yk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
