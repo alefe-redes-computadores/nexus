6cb0423e2639f3dec27ff886d602ac5a5c22defb
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-08-06 13:54:07 -0300

    Atualizar o page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useState } from 'react';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '../lib/supabase';
 import { Mail, Lock, ArrowRight } from 'lucide-react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
