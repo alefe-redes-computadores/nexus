cdcddb3dbc120df2d4d47b56dab3d244b36a90c4
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-08-06 14:10:16 -0300

    Atualizar o page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 import Image from 'next/image';
 
 export default function LoginPage() {
